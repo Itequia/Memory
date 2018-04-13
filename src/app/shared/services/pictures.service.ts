39d79b4fb3119eb629bb2d7187e9e5b70c489298
David Canut <david.canut@itequia.com>
2018-04-13 13:15:13 +0200

    conflicts

--- src/app/shared/services/pictures.service.ts
+++ src/app/shared/services/pictures.service.ts
@@ -3,34 +3,34 @@ import { Injectable } from '@angular/core'
 const app = window['remote'].app
 
 @Injectable()
-export class PicturesService {  
-    
-    private _tempFileName: string = "/memory-temp.txt"
-    private _allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
+export class PicturesService {
 
-    private _tempFilePath: string
+	private readonly _tempFileName: string = '/memory-temp.txt'
+	private readonly _allowedExtensions: RegExp = /(\.jpg|\.jpeg|\.png)$/i
 
-    constructor() {
-        this._tempFilePath = app.getPath('userData') + this._tempFileName
-        console.log(this._tempFilePath)
-    }
-    
-    setPictures(folder: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            window['fs'].readdir(folder, (error, files) => {
-                if (error) reject(error)
+	private _tempFilePath: string
 
-                let images = files.filter(file => this._allowedExtensions.exec(file))
-                
-                window['fs'].writeFile(this._tempFilePath, images, (err) => {
-                    if (err) reject(error)
-                    resolve()
-                }); 
-            })
-        })
-    }
+	constructor() {
+		this._tempFilePath = app.getPath('userData') + this._tempFileName
+		console.log(this._tempFilePath)
+	}
 
-    getPictures(): Promise<string[]> {
-        return Promise.resolve(['','','','','','',''])
-    }
+	setPictures(folder: string): Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			window['fs'].readdir(folder, (error, files) => {
+				if (error) reject(error)
+
+				const images = files.filter(file => this._allowedExtensions.exec(file))
+
+				window['fs'].writeFile(this._tempFilePath, images, (err) => {
+					if (err) reject(error)
+					resolve()
+				})
+			})
+		})
+	}
+
+	getPictures(): Promise<string[]> {
+		return Promise.resolve(['', '', '', '', '', '', ''])
+	}
 }
